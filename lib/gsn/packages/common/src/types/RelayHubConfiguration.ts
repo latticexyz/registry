fac26cfeeb8413a4b464cda075c0ece684ee8d60
Scott Moses Sunarto <github@smsunarto.com>
2022-02-17 21:44:03 -0800

    💽️: git submodules cleanup

--- lib/gsn/packages/common/src/types/RelayHubConfiguration.ts
+++
@@ -1,9 +0,0 @@
-export interface RelayHubConfiguration {
-  maxWorkerCount: number | BN
-  gasReserve: number | BN
-  postOverhead: number | BN
-  gasOverhead: number | BN
-  minimumUnstakeDelay: number | BN
-  devAddress: string
-  devFee: number | BN | string
-}
